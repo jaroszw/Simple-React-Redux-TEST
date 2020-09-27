b444418d27591d81d916c51f039d2c54f89af72a
Wojciech Jarosz <jaroszw@gmail.com>
2020-09-27 12:15:53 +0200

    Redux loggIn, Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, logged} from './actions';
 
 function App() {
+
+  const count = useSelector(state => state.counter)
+  const log = useSelector(state => state.logged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(logged())}>LOG</button>
+      {log?<h3>Jakiś tajny text</h3> : '' }
     </div>
   );
 }
